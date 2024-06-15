3a959f4c9c72af7938e7e4825f6695f6c790739c
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-06-15 18:11:50 -0300

    feat: Update homepage layout and order of elements

--- app/page.jsx
+++ app/page.jsx
@@ -9,9 +9,9 @@ const Home = () => {
       <div className="container mx-auto h-full">
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
       {/* texto */}
-      <div className="text-center xl:text-left">
+      <div className="text-center xl:text-left order-2 xl:order-none">
         <span className="text-xl">Engenheiro de Software</span>
-        <h1 className="h1">
+        <h1 className="h1 mb-6">
           Olá Eu sou <br/> <span className="text-accent">Jonathas Vinicius</span>
         </h1>
         <p className="max-w-[500px] mb-9 text-white/80"> 
@@ -36,7 +36,7 @@ const Home = () => {
         </div>
       </div>
       {/* foto */}
-      <div>
+      <div className="order-1 xl:order-none mb-8 xl:mb-0">
         <Photo />
       </div>
       </div>
