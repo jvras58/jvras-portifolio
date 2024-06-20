f26a9ceabe16378b85f2d0f9785b82ef950f93cc
jvras <jvras@cin.ufpe.br>
2024-06-20 19:02:32 -0300

    wip: work page to improve project display

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -8,7 +8,7 @@ import "swiper/css";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
-import { Tooltip, TooltipTrigger, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -16,20 +16,20 @@ import Image from "next/image";
 const projects= [
     {
         num: "1",
-        category: "frontend",
+        category: "frontend project",
         title: "Project 1",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        stack: [{name: "Lorem"}, {name: "ipsum dolor"}, {name: "sit amet"}],
+        stack: [{name: "Stack1"}, {name: "Stack2"}, {name: "Stack3"}],
         image: "/assets/work/thumb1.png",
         live: "",
         github: "",
     },
     {
         num: "2",
-        category: "backend",
+        category: "backend project",
         title: "Project 2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        stack: [{name: "Lorem2"}, {name: "ipsum dolor2"}, {name: "sit amet2"}],
+        stack: [{name: "Stack1"}, {name: "Stack2"}, {name: "Stack3"}],
         image: "/assets/work/thumb1.png",
         live: "",
         github: "",
@@ -55,7 +55,24 @@ const Work = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-                        text
+                        <div>
+                            {/* outline num */}
+                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                                {project.num}
+                            </div>
+                            {/* category */}
+                            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                                {project.category}
+                            </h2>
+                            {/* descrition */}
+                            <p className="text-white/60">{project.description}</p>
+                            {/* stack */}
+                            <ul className="flex gap-4">
+                               {project.stack.map((item, index) => {
+                                    return <li key={index} className="text-white/60">{item.name}</li>
+                                })}
+                            </ul>
+                        </div>
                     </div>
                     <div className="w-full xl:w-[50%]">slide</div>
                 </div>
