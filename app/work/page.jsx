25e3027763e8eb41a753f19252bf8a8d5cd1885b
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-06-21 09:13:29 -0300

    wip: botões de passar slides na page de works

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -8,10 +8,11 @@ import "swiper/css";
 
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects= [
     {
@@ -29,7 +30,7 @@ const projects= [
         category: "backend project",
         title: "Project 2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        stack: [{name: "Stack1"}, {name: "Stack2"}, {name: "Stack3"}],
+        stack: [{name: "Stack11"}, {name: "Stack22"}, {name: "Stack33"}],
         image: "/assets/work/thumb1.png",
         live: "",
         github: "",
@@ -55,7 +56,7 @@ const Work = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-                        <div>
+                        <div className="flex flex-col gap-[30px] h-[50%]">
                             {/* outline num */}
                             <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                                 {project.num}
@@ -68,13 +69,81 @@ const Work = () => {
                             <p className="text-white/60">{project.description}</p>
                             {/* stack */}
                             <ul className="flex gap-4">
-                               {project.stack.map((item, index) => {
-                                    return <li key={index} className="text-white/60">{item.name}</li>
+                            {project.stack.map((item, index) => {
+                                    return( 
+                                    <li key={index} className="text-xl text-accent">
+                                        {item.name}
+                                        {/* remove the last commo*/}
+                                        {index !== project.stack.length - 1 && ","}
+                                    </li>
+                                
+                                );
                                 })}
                             </ul>
+                            {/* border */}
+                            <div className="border border-white/20"></div>
+                            {/* buttons*/}
+                            <div className="flex items-center gap-4">
+                                {/* live button */}
+                                <Link href={project.live}>
+                                <TooltipProvider delayDuration={100}>
+                                    <Tooltip>
+                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>Live Projects</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                                </Link>
+                                {/* github project button */}
+                                <Link href={project.github}>
+                                <TooltipProvider delayDuration={100}>
+                                    <Tooltip>
+                                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>Github Repository</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                    <div className="w-full xl:w-[50%]">slide</div>
+                    <div className="w-full xl:w-[50%]">
+                    <Swiper
+                    spaceBetween={30}
+                    slidesPerView={1}
+                    className="xl:h-[520px] mb-12"
+                    onSlideChange={handleSlideChange}
+                    >
+                        {projects.map((project, index) => {
+                            return (
+                                <SwiperSlide key={index} className="w-full">
+                                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                                    {/* overloy */}
+                                    <div></div>
+                                    {/* image */}
+                                    <div className="relative w-full h-full">
+                                        <Image
+                                        src={project.image}
+                                        fill 
+                                        className="object-cover"
+                                        alt=""
+                                        />
+                                    </div>
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
+                        <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 battom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" 
+                        btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center rounded-full transition-all"
+                        />
+                    </Swiper>
+                    </div>
                 </div>
             </div>
         </motion.section>
---
+++ components/WorkSliderBtns.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+const WorkSliderBtns = ({containerStyles, btnStyles, iconsStyle}) => {
+    const swiper = useSwiper();
+    return (
+        <div className={containerStyles}>
+        <button className={btnStyles} onClick={() => swiper.slidePrev}>
+            <PiCaretLeftBold className={iconsStyle} />
+        </button>
+        <button className={btnStyles} onClick={() => swiper.slideNext}>
+            <PiCaretRightBold className={iconsStyle} />
+        </button>
+        
+        </div>
+    )
+}
+export default WorkSliderBtns;
