0d88a9bfc4979442fe27bab50e0e7e516605ebc0
Jonathas Vinicius <jonathas.vinicius@ufpe.br>
2024-10-13 13:54:16 -0300

    feat: add G1 news pages for G1 Pernambuco and G1 global

---
+++ app/g1/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+
+const g1 = () => {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+        <iframe
+            src="https://g1.globo.com/pe/pernambuco/"
+            title="G1 News"
+            className="w-full h-[600px] border-none"
+        />
+        </div>
+    );
+}
+
+export default g1;
